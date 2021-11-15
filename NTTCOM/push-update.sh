f548f19b60c70ecb55680477dcc1b04246b70e1f
MIDnet Engineering <eng@mid.net>
2021-11-14 21:40:40 -0600

    update script for NTTCOM

---
+++ NTTCOM/push-update.sh
@@ -0,0 +1,28 @@
+#!/bin/env bash
+
+# add some crap here to die if $1 is not defined
+# maybe add --from and --reply-to options?
+# for that matter, --sign-as? would have to be in the keyring of the running user, though.
+
+WORKDIR=$1
+
+AUTODBM="auto-dbm@rr.ntt.net"
+FROM="noc@mid.net"
+REPLYTO="noc@mid.net"
+
+cd $WORKDIR
+
+while read OBJ ; do
+	/usr/bin/cat $OBJ >> $WORKDIR/obj-updates.txt
+	echo " " >> $WORKDIR/obj-updates.txt
+done <<< `ls`
+
+echo "updates file"
+/usr/bin/cat $WORKDIR/obj-updates.txt
+echo " "
+echo "signing updates file"
+/usr/bin/gpg --clearsign $WORKDIR/obj-updates.txt
+echo " "
+echo "sending mail"
+echo "/usr/bin/cat $WORKDIR/obj-updates.txt.asc | /usr/bin/mailx -s "updates" -r "MIDnet NOC" <noc@mid.net> -S replyto="noc@mid.net" $AUTODBM"
+/usr/bin/rm $WORKDIR/obj-*
